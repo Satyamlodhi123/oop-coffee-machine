f1fdb12545d93c169d4abdffa8162675f4aff549
satyam singh lodhi <satyamlodhi123@gmail.com>
2025-08-21 20:17:28 +0530

    final code

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ while machine_on:
     items = Menu()
     # takes the input of the drink name
     drink_name = input(f"what would you like? {items.get_items()}")
+    if drink_name  == 'report':
+        money.report()
+    elif drink_name == 'off':
+        machine_on = False
     #return a menu item if the name exits
-    drink = items.find_drink(drink_name)
-    coffee__maker = CoffeeMaker()
-    if money.make_payment(drink.cost):
-        if coffee__maker.is_resource_sufficient(drink):
-            coffee__maker.make_coffee(drink)
+    else:
+        drink = items.find_drink(drink_name)
+        coffee__maker = CoffeeMaker()
+        if money.make_payment(drink.cost):
+            if coffee__maker.is_resource_sufficient(drink):
+                coffee__maker.make_coffee(drink)
 
 
 
